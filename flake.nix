8ba21eeabc81f5f829171a89f6aeb92fbad11cd9
desktop1 <desktop1@battle.localdomain>
2024-11-24 14:27:58 -0500

    go

--- flake.nix
+++ flake.nix
@@ -1,14 +1,11 @@
 {
-
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { ... }: {
-    nixosModules = {
-      pine64-pinephonepro = import ./devices/pine64-pinephonepro;
-      module-list = import ./modules/module-list.nix;
-    };
+    nixosModules = [
+      ./devices/pine64-pinephonepro
+      ./modules/module-list.nix
+    ];
   };
 }
-
 
